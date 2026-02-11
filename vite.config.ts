f7b07e9507a02dce973b0b9b0fb24df3c6001d05
v-nesh <nesh.baskaran@gmail.com>
2026-02-12 01:13:03 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/portfolio/",
   server: {
     host: "::",
     port: 5173,
